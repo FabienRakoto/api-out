106f4e16b3545901f5e45b1113a3db67dd2372ec
Fabien Arthur <zigiiefabienarthur@gmail.com>
2019-07-08 09:25:31 +0300

    in and out finished

--- functions/index.js
+++ functions/index.js
@@ -20,27 +20,50 @@ exports.getUsers = functions.https.onRequest( (request, response) => {
             const user = doc.data();
             user['key'] = key;
             users[key] = user
-        });
-    
-         
+        });     
         CSVToJSON().fromFile('./source.csv').then(source => {
 
             for(const key in users){
                 source.push(users[key])
             }
-             var csv = JSONToCSV(source, { fields : ["email","username","key"]});
-             FileSystem.writeFileSync("./destination.csv", csv);
 
+             const csv = JSONToCSV(source, { fields : ["email","username","key"]});
+            
+             FileSystem.writeFileSync("./destination.csv",   csv, {mode : parseInt(777, 8)});
              response.send(source)
              return null
- 
+
          }).catch(error => {
-             response.status(500).send(error)
+            console.log(error)
+            response.send(error)
          })
 
+         return null
 
     }) .catch(reason => {
         response.status(500).send('db.collection("users").get gets err, reason: ' + reason);
     });
+})
+
+exports.addUser = functions.https.onRequest((request, response) => {
+    CSVToJSON().fromFile('./add.csv').then(source => {
+
+        for(const key in source){
+            db
+            .collection("users")
+            .add(source[key])
+            .then(ref => {
+              return response.status(200).send(ref.id)
+            }).catch(error => {
+                response.status(500).send("somethings went wrongs", error)
+            })
+        }
+        
+         return null
+         
+     }).catch(error => {
+         response.status(500).send(error)
+     })
+
 })
 
