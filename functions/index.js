b57afebcde84bb754eb87ad29915c60adce14d1d
Fabien Arthur <zigiiefabienarthur@gmail.com>
2019-07-09 12:26:46 +0300

    api-out finished

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,24 @@ const CSVToJSON = require('csvtojson');
 const JSONToCSV = require('json2csv').parse;
 const FileSystem = require('fs');
 const serviceAccount = require('./key.json');
-const PromiseFtp = require('promise-ftp');
+const FtpDeploy = require('ftp-deploy')
 admin.initializeApp({
     credential : admin.credential.cert(serviceAccount),
     databaseURL :"https://fir-js-29865.firebaseio.com/"
 })
+
+const config  = {
+    user: "iziweb",                   // NOTE that this was username in 1.x 
+    password: "izi2014z",           // optional, prompted if none given
+    host: "198.50.210.81",
+    port: 21,
+    localRoot: __dirname + '/upload',
+    remoteRoot: '/csv-out/',
+    include: ['*.csv', 'upload/*'], // include: ['*', '**/*'],      // this would upload everything except dot files
+    exclude: ['dist/**/*.map'],     // e.g. exclude sourcemaps - ** exclude: [] if nothing to exclude ** // delete ALL existing files at destination before uploading, if true
+    deleteRemote: false,              
+    forcePasv: true
+   }
 
 const db = admin.firestore()
 
@@ -29,8 +42,12 @@ exports.getUsers = functions.https.onRequest( (request, response) => {
 
              const csv = JSONToCSV(source, { fields : ["email","username","key"]});
             
-             FileSystem.writeFileSync("./destination.csv",   csv, {mode : parseInt(777, 8)});
-             response.send(source)
+             FileSystem.writeFileSync("./upload/destination.csv", csv);
+             const ftpDeploy = new FtpDeploy();
+             ftpDeploy.deploy(config)
+              .then(res => console.log('finished:', res))
+              .catch(err => console.log(err))
+            // response.send(source)
              return null
 
          }).catch(error => {
@@ -93,16 +110,5 @@ exports.verifyUser = functions.https.onRequest((request, response) => {
     }).catch(error => {
         response.status(500).send(error)
     })
-})
-
-exports.configFtp = functions.https.onRequest((request, response) => {
-    var ftp = new PromiseFtp();
-    ftp.connect({host: "198.50.210.81", user: "iziweb", password: "izi2014z"})
-    .then(function (serverMessage) {
-        response.send(serverMessage)
-        return ftp.put('./destination.csv', '/kaylandko');
-      }).then(function () {
-        return ftp.end();
-      });
 })
 
